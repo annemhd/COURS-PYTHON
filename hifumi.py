622d6db938c20469caaa83509e3517a62332955a
Anne-Catherine MICHAUD <71764180+annemhd@users.noreply.github.com>
2021-11-05 15:26:26 +0100

    Màj 05/11/21

--- hifumi.py
+++ hifumi.py
@@ -4,8 +4,10 @@ import random
 # Données
 scoreUser       = 0
 scoreComputer   = 0
-options = ["Pierre", "Feuille", "Ciseaux"]
+options         = ["Pierre", "Feuille", "Ciseaux"]
+separator       = "-------------------------------------------------------------------"
 
+# La partie continue jusqu'à ce qu'un joueur atteint le score de 3
 while True :
     # L'utilisateur commence la partie
     choiceUser = input("- Pierre, Feuille ou Ciseaux ? ")
@@ -15,12 +17,13 @@ while True :
     choiceComputer = options[randomOption]
 
 
-    # Posibilité avec la Pierre
+    # Posibilités avec la Pierre
     if choiceUser == "Pierre" and choiceComputer == "Pierre":
         print("- Vous avez choisi " + choiceUser)
         print("- La machine à choisi " + choiceComputer)
         print("- Match nul")
         print("Vous " + str(scoreUser) + " - " + str(scoreComputer) + " Ordinateur")
+        print(separator)
 
     elif choiceUser == "Pierre" and choiceComputer == "Feuille":
         scoreComputer += 1
@@ -28,6 +31,7 @@ while True :
         print("- La machine à choisi " + choiceComputer)
         print("- La machine a gagné")
         print("Vous " + str(scoreUser) + " - " + str(scoreComputer) + " Ordinateur")
+        print(separator)
 
     elif choiceUser == "Pierre" and choiceComputer == "Ciseaux":
         scoreUser += 1
@@ -35,16 +39,17 @@ while True :
         print("- La machine à choisi " + choiceComputer)
         print("- Vous avez gagné")
         print("Vous " + str(scoreUser) + " - " + str(scoreComputer) + " Ordinateur")
+        print(separator)
 
 
 
-
-    # Posibilité avec la Feuille
+    # Posibilités avec la Feuille
     elif choiceUser == "Feuille" and choiceComputer == "Feuille":
         print("- Vous avez choisi " + choiceUser)
         print("- La machine à choisi " + choiceComputer)
         print("- Match nul")
         print("Vous " + str(scoreUser) + " - " + str(scoreComputer))
+        print(separator)
 
     elif choiceUser == "Feuille" and choiceComputer == "Pierre":
         scoreUser += 1
@@ -52,6 +57,7 @@ while True :
         print("- La machine à choisi " + choiceComputer)
         print("- Vous avez gagné")
         print("Vous " + str(scoreUser) + " - " + str(scoreComputer) + " Ordinateur")
+        print(separator)
 
     elif choiceUser == "Pierre" and choiceComputer == "Ciseaux":
         scoreUser += 1
@@ -59,16 +65,17 @@ while True :
         print("- La machine à choisi " + choiceComputer)
         print("- Vous avez gagné")
         print("Vous " + str(scoreUser) + " - " + str(scoreComputer) + " Ordinateur")
+        print(separator)
 
 
 
-
-    # Posibilité avec la Ciseau
+    # Posibilités avec la Ciseau
     elif choiceUser == "Ciseaux" and choiceComputer == "Ciseaux":
         print("- Vous avez choisi " + choiceUser)
         print("- La machine à choisi " + choiceComputer)
         print("- Match nul")
         print("Vous " + str(scoreUser) + " - " + str(scoreComputer) + " Ordinateur")
+        print(separator)
 
     elif choiceUser == "Ciseaux" and choiceComputer == "Pierre":
         scoreComputer += 1
@@ -76,6 +83,7 @@ while True :
         print("- La machine à choisi " + choiceComputer)
         print("- Vous avez perdu")
         print("Vous " + str(scoreUser) + " - " + str(scoreComputer) + " Ordinateur")
+        print(separator)
 
     elif choiceUser == "Ciseaux" and choiceComputer == "Feuille":
         scoreUser += 1
@@ -83,10 +91,27 @@ while True :
         print("- La machine à choisi " + choiceComputer)
         print("- Vous avez gagné")
         print("Vous " + str(scoreUser) + " - " + str(scoreComputer) + " Ordinateur")
+        print(separator)
 
+
+    # Si la réponse est au mauvais format (Pas de majuscule au début du mot. ex: pierre = incorrect)
     else:
         print("- " + choiceUser + " est une réponse incorrecte")
+        print(separator)
 
+
+    # Fin de la partie, quand un des deux joueurs à atteint le scrore de 3
     if (scoreUser == 3 or scoreComputer == 3 ):
-        print("fin")
+
+        # Message à afficher pour le gagnat ou le perdant
+        if (scoreComputer == 3):
+            print("VOUS AVEZ PERDU...")
+        else:
+            print("FÉLICITATION, VOUS AVEZ GAGNÉ !")
+
+        print("- Fin de la partie")
+        print("- Pour recommencer, relancer la cmd : ")
+        print("- py hifumi.py")
+        print("- ou")
+        print("- python3 hifumi.py")
         break
